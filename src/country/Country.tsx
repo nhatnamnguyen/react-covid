7c8dfd86b24cd94251c45e4c0ed13ebcc8738e2f
Nhat Nam NGUYEN <nguyennhatnam2010@gmail.com>
2020-04-12 15:06:17 +0200

    favicon

--- src/country/Country.tsx
+++ src/country/Country.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CountryItem from './CountryItem'
 
 function Country() {
 
+    const [countryResults, setCountryResults] = useState([{country: '', cases: 0, deaths: 0, recovered: 0 }])
+
+    useEffect(() => {
+        fetch('https://coronavirus-19-api.herokuapp.com/countries')
+            .then(response => {
+                return response.json()
+            }).then(data => {
+                console.log("data: " + data)
+                setCountryResults(data)
+            })
+    }, [])
+
+    const listItems = countryResults
+        .filter(countryResult => {
+            return countryResult.country !== 'Total:' 
+                && countryResult.country !== ''
+                && countryResult.country !== 'World'                
+        }).map((countryResult) =>
+            <div>
+                <CountryItem countryName={countryResult.country} case={countryResult.cases} death={countryResult.deaths} recovered={countryResult.recovered} />
+                <p></p>
+            </div>
+        );
     return (
         <div>
-            <div>CoronaVirus by country</div>
-            <CountryItem countryName='France' case={129654} death={13832} recovered={26391} />
+            {listItems}
         </div>
     )
 }
