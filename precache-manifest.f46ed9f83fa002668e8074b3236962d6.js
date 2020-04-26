3ff80146c0f91341f8845bfda4a0547a3ce57902
nhatnam <nguyennhatnam2010@gmail.com>
2020-04-26 09:59:21 +0200

    Updates

---
+++ precache-manifest.f46ed9f83fa002668e8074b3236962d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5a21bbd3d4d78c814269b94452735e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e17aaa64624a039a740",
+    "url": "/static/css/main.e2d64ccf.chunk.css"
+  },
+  {
+    "revision": "b97d95ce75bd02a1530c",
+    "url": "/static/js/2.905a0c6b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.905a0c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e17aaa64624a039a740",
+    "url": "/static/js/main.8ba77e7b.chunk.js"
+  },
+  {
+    "revision": "c81a40a15f69f031fc29",
+    "url": "/static/js/runtime-main.92436ac7.js"
+  }
+]);
