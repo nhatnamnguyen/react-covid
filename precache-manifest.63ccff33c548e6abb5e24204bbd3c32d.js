e4200841bdd5acbd731266211ec22810d54eedea
Nhat Nam NGUYEN <nguyennhatnam2010@gmail.com>
2021-03-19 15:20:58 +0100

    Add files via upload

---
+++ precache-manifest.63ccff33c548e6abb5e24204bbd3c32d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200103443e921443826268eae9bd4102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23972a2fa9e82d490f6f",
+    "url": "/static/css/main.8c16ab4d.chunk.css"
+  },
+  {
+    "revision": "fde471956bcb18f31b4a",
+    "url": "/static/js/2.ea39bb43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ea39bb43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23972a2fa9e82d490f6f",
+    "url": "/static/js/main.1626a46a.chunk.js"
+  },
+  {
+    "revision": "c81a40a15f69f031fc29",
+    "url": "/static/js/runtime-main.92436ac7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f46ed9f83fa002668e8074b3236962d6.js"
+  "/precache-manifest.63ccff33c548e6abb5e24204bbd3c32d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
